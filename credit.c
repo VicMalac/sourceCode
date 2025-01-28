7b585b8ba47722cc35c7b357739abf8d747db6d4
VicMalac <victorpdpyt9098@gmail.com>
2025-01-28 12:49:03 -0300

    Melhora de código

--- credit.c
+++ credit.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(void){
-    long long i = 3787344936712345; // Número do cartão
+    long long i = 378282246310005; // Número do cartão
 
     // Vendo qual a ordem do digito do cartão 
-    long long n16 = i % 10; 
+    long long n16 = i % 10;
     long long n15 = (i % 100) / 10;
     long long n14 = (i % 1000) / 100;
     long long n13 = (i % 10000) / 1000;
@@ -21,9 +21,29 @@ int main(void){
     long long n2 = (i % 1000000000000000) / 100000000000000;
     long long n1 = (i % 10000000000000000) / 1000000000000000;
 
+    if (n1 == 0 && n2 == 0 && n3 == 0 && n4 == 4){
+        n1 = n4; n2 = n5; n3 = n6; n4 = n7; n5 = n8; n6 = n9; n7 = n10; n8 = n11; n9 = n12; n10 = n13; n11 = n14; n12 = n15; n13 = n16; n14 = 0; n15 = 0; n16 = 0;
+        int temp = 15;
+        char banco[15] = "VISA";
+        printf("%s \n", banco);
+    }
+    else{
+        if (n1 == 0 && n2 == 3 && (n3 == 4 || n3 == 7)){
+            n1 = n2; n2 = n3; n3 = n4; n4 = n5; n5 = n6; n6 = n7; n7 = n8; n8 = n9; n9 = n10; n10 = n11; n11 = n12; n12 = n13; n13 = n14; n14 = n15 = n16; n16 = 0;
+            char * banco = "AMEX"; //Amex tem 15 dígitos
+            printf("%s \n", banco);
+        }
+        else{
+            if (n1 == 5 && (n2 == 1 || n2 == 2 || n2 == 3 || n2 == 4 || n2 == 5)){
+                printf("MASTERCARD \n");
+            }
+        }
+    }
+
+
 
 
     // Exibe os números do cartão e a soma modifcação
     printf("%lld %lld %lld %lld %lld %lld %lld %lld %lld %lld %lld %lld %lld %lld %lld %lld \n", 
-           n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13, n14, n15, n16); //entendi
+           n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13, n14, n15, n16);
 }
