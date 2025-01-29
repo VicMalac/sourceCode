63af5cf03db259cfd3791dcfb0dc33c06a4c8ec7
Victor <victorpdpyt9098@gmail.com>
2025-01-29 20:45:28 -0300

    .

--- Modulo 1/population.c
+++ Modulo 1/population.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
 
 int main(void) {
-    int init = 10;
-    int end, grow = 0, dead, i = 0;
+    int init, end, grow = 0, dead, i = 0;
     do{
         printf("Start Size: ");
         scanf("%d", &init);
-        //printf("Population End: ");
-        //scanf("%d", &end);
-    }while(init < 9 || init == end || init > end);
+    }while(init < 9);
     
     do{
         printf("End Size: ");
         scanf("%d", &end);
-    }while(init < 9 || init == end || init > end);
+    }while(init > end);
 
     while (init < end){
         grow = init / 3;
         dead = init / 4;
         init += grow - dead;
-        i = i + 1;
+        i++;
     }
     printf("Years: %d\n", i);
 }
