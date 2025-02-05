7e7beaed42bdb2cfeb131173759850e7e383a759
Victor <victorpdpyt9098@gmail.com>
2025-02-05 20:41:18 -0300

    New

---
+++ Modulo_2/brick.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void){
+    char hash = '#';
+
+    printf("%f", (float) hash);
+}
--- Modulo_2/function.c
+++ Modulo_2/function.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-const int total = 3; //Numero fixo de notas
+const int total = 3; //Constante: Número fixo que não pode ser mudado
+float average(); //Ativa uma função que está mais abaixo no código
+
 int main(void){
-    int notas[3];
-    for (int i = 0; i < total; i++){
+    system("cls");
+    int notas[3]; //Declara um array chamado notas com 3 variações diferentes
+    for (int i = 0; i < total; i++){ //Faz a repetição quantas vezes as constante mandar nos arrays
         printf("Digite a nota: ");
-        scanf("%f", &notas[i]);
+        scanf("%d", &notas[i]);
     }
 
-    printf("Media: %f\n", (notas[0] + notas[1] + notas[2]) / 3.0);
+    printf("Media: %.2f\n", average(total, notas)); //Imprime o que estiver dentra da função average
 }
+float average(int length, int array[]){
+    int sum = 0;
+    for(int i = 0; i < length; i++){
+        sum = sum + array[i]; //Soma cada um dos arrays entre si
+    }
+    return sum / (float) length; //Retorna o valor da soma dividido por length para a função average, ou seja, average = media
+    }
