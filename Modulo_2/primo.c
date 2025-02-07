9193c60c9f56762d48f9f361e4b3d882debea993
Victor <victorpdpyt9098@gmail.com>
2025-02-06 22:44:13 -0300

    Correct codes

--- Modulo_2/primo.c
+++ Modulo_2/primo.c
@@ -22,7 +22,7 @@ int main(){
     int i = 3;
     printf("Digite um numero para descobrir se ele e primo ou nao: ");
     scanf("%d", &i);
-    if(i % 2 == 0 && i % 3 == 0 && i % 5 == 0 && i % 7 == 0 && i % 11 == 0 && i % 13 == 0 && i % 17 == 0 && i % 19 == 0 && i % 23 == 0){
+    if(i % 2 == 0 || i % 3 == 0 || i % 5 == 0 || i % 7 == 0 || i % 11 == 0 || i % 13 == 0 || i % 17 == 0 || i % 19 == 0 || i % 23 == 0){
         system("cls");
         printf("%d nao e um numero primo.", i);
     }
