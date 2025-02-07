9cfd2ab4aaf2d140f72d8ab27202d2fa4fd87231
Victor <victorpdpyt9098@gmail.com>
2025-02-07 00:10:35 -0300

    Modification in Arrays
    New codes and sintaxes of C

--- Modulo_2/string.c
+++ Modulo_2/string.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
-
-
-
-//corrigir
-
-int main (void){
-    char *s;
+#include <string.h>
+int main(void){
+    char s[100]; //Declara o array com tamanho até 100
     printf("Input: ");
-    scanf("%ms", s);
-    printf("Output: ");
-
-    for (int i = 0; s[i] != '\0'; i++) {
+    scanf("%[^\n]", s); //Pega um input utilizando %[^\n], tem qu ser dessa forma, tenho que entender
+    printf("Output: "); //int n = strlen(s); Aplica o valor de strlen() para n, isso facilita no processamento
+    for (int i = 0, n = strlen(s); i < n; i++) { //Se strlen() estivesse aqui seria desperdício de processamento //strlen() não precisa receber um tipo como int, pois no int i já declarou como int para toda a estrutura for
         printf("%c", s[i]);  // Imprime cada caractere da string
     }
 }
