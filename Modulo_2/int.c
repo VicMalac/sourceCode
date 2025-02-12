33e2a2addb1d01bfffd6364f7763329b66ffa4d2
VicMalac <victorpdpyt9098@gmail.com>
2025-02-12 11:35:31 -0300

    Melhora no código
    Fiz algumas modificações para deixar a saída no terminal ficar mais bonita e com menos informações

--- Modulo_2/int.c
+++ Modulo_2/int.c
@@ -1,7 +1,10 @@
 //Soma de Elementos de um Array: Crie um programa que leia 5 números inteiros em um vetor e calcule a soma de todos os elementos
+//Exercício completo
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void){
+    system("cls");
     int qta;
     printf("Quantos numeros deseja que sejam somados: ");
     scanf("%d", &qta);
@@ -12,5 +15,6 @@ int main(void){
         scanf("%d", &intarray[i]);
         sum += intarray[i];
     }
+    system("cls");
     printf("A soma de todos esses numeros e: %d", sum);
 }
