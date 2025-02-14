1ca137d9a86b8ba6cbac612e953660139a83a26b
Victor <victorpdpyt9098@gmail.com>
2025-02-13 23:37:46 -0300

    fibonacci

---
+++ Modulo_2/fibanacci.c
@@ -0,0 +1,18 @@
+//Números Fibonacci: Escreva um programa que leia um número N e imprima os N primeiros números da sequência Fibonacci.
+//1, 1, 2, 3, 5, 8, 13, 21, 34, 55 /////// caso n = 10
+//x, y, x, y
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    system("cls");
+    int n = 0, x = 1, y = 1, sum;
+    printf("Digite um numero: ");
+    scanf("%i", &n);
+    for (int i = 1; i <= n; i+=2){
+        printf("%i %i ", x, y);
+        x += y;
+        y += x;
+    }
+}
