5d4b863b93bfafaa6423cac2efe0b4b7d88489e0
Victor <victorpdpyt9098@gmail.com>
2025-02-27 23:34:25 -0300

    End of the module 2
    I created the last exercise code in C to encrypt a text using a key.

--- Modulo_2/substitution.c
+++ Modulo_2/substitution.c
@@ -1,40 +1,24 @@
-/*Implemente um programa que implemente uma cifra de substituição, conforme a seguir.
-
-$ ./substitution JTREKYAVOGDXPSNCUIZLFBMWHQ
-texto simples: HELLO
-texto cifrado: VKXXN*/
-
-//Get the key / Pega uma chave
-//* Validate the key / Valida essa chave
-//? Get the plaintext / Pega o texto que quero codificar
-//TODO Encipher / Codifica
-//! Print ciphertext / Imprime na tela o texto codificado
-
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
 
 int main(void){
-    char alfa[26], texto[100]; //Aceita somente 26 caracteres
-    char ciframai[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O','P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-    char ciframin[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o','p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    char alfa[30], texto[100]; //Aceita somente 26 caracteres
+    char ciframai[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+    char ciframin[] = {"abcdefghijklmnopqrstuvwxyz"};
     int var;
-
     
-
+    do {
+        printf("Digite a sua versao do alfabeto: ");
+        fgets(alfa, sizeof(alfa), stdin); 
+    } while (isdigit(alfa));
+    
     printf("Texto que sera criptografado: ");
-    fgets(texto, sizeof(texto), stdin); //text[i] == 'h'
-    printf("Digite a sua versao do alfabeto: ");
-    scanf("%s", alfa);
+    fgets(texto, sizeof(texto), stdin);
 
-    int num = 0; //tentar com while depois
+    int num = 0;
 
     for (int i = 0, n = strlen(texto); i < n; i ++){
-        //Fazer uma condição para ver se é uma letra espaço ou número, se não for letra nada muda, se for é só mudar//texto que vou decifrar é abc para xyz
-        //if (texto[i] == ' ' || texto[i] == '!', || texto[i] == '.' || texto[i] == ',' || texto[i] == '?')
-
-        //se for numero ou espaço não faço nada
-            // Primeiro verifica se é um caractere que deve ser mantido
         if (texto[i] == ' ' || texto[i] == '!' || texto[i] == '?' || texto[i] == '.' || texto[i] == ',' || isdigit(texto[i])) {
             printf("%c", texto[i]);
             continue;
@@ -46,20 +30,16 @@ int main(void){
                 num++;
                 }
 
-            printf("%c", alfa[num]);
+            printf("%c", tolower(alfa[num]));
         }
         else{
+            if(texto[i] >= 'A' && texto[i] <= 'Z'){
                 num = 0;
-                while (ciframai[num] != toupper(texto[i])){
+                while (ciframin[num]!= tolower(texto[i])){
                     num++;
                     }
-        
                 printf("%c", toupper(alfa[num]));
+            }
         }
-        //Aqui preciso fazer alguma forma de mudar a letra, se eu digitar cdb na key, e digitar abc no input, deve aparecer cdb
-
-        //se o a letra for identica você executa, se não pula para a proxima, ele verifica o alfa[0]
-        //Se eu pego uma letra ex: n, eu faço uma verificação, no ciframin, ciframin[0] == texto[0]? se sim, uma variavel, recebe 0
-        //senão, proximo, ciframin[1] == texto[0], se sim, uma variavel, recebe 1 se não, proximo
     }
 }
